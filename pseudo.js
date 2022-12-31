b059a9c330c290b4191280ffaccef2105f21f8d5
Christian Yuen <christianyuen1@gmail.com>
2022-12-31 18:06:52 -0500

    Appended Pokemon Name & Image + Added Button Event Listener

--- pseudo.js
+++ pseudo.js
@@ -1,90 +1,92 @@
 // create an pokemonGenerator object and intialize function
 const pokemonGenerator = {};
 
-pokemonGenerator.name = [];
+// event listener
+const button = document.querySelector('button');
 
-pokemonGenerator.photo = [];
+button.addEventListener('click', function(event) {
+console.log(event);
 
 // API url
-pokemonGenerator.url = "https://pokeapi.co/api/v2/pokemon/";
+  pokemonGenerator.url = "https://pokeapi.co/api/v2/pokemon/";
 
-// create a getData function
-pokemonGenerator.getData = () => {
-  // construct the url
-  const url = new URL(pokemonGenerator.url);
-  url.search = new URLSearchParams({
-    url: "url",
-    limit: 248,
-  });
-
-  // fetch from url
-  fetch(url)
-    .then((response) => {
-      // convert to .json
-      return response.json();
-    })
-    .then((jsonData) => {
-      // save data to namespace object
-      pokemonGenerator.data = jsonData;
-      // call function to display names
-      pokemonGenerator.displayPokemon();
-
-      console.log(pokemonGenerator.data);
+  // create a getData function
+  pokemonGenerator.getData = () => {
+    // construct the url
+    const url = new URL(pokemonGenerator.url);
+    url.search = new URLSearchParams({
+      url: "url",
+      limit: 248,
     });
-};
 
-// displayData();
-pokemonGenerator.displayPokemon = () => {
+    // fetch from url
+    fetch(url)
+      .then((response) => {
+        // convert to .json
+        return response.json();
+      })
+      .then((jsonData) => {
+        // save data to namespace object
+        pokemonGenerator.data = jsonData;
+        // call function to display names
+        pokemonGenerator.displayPokemon();
+
+        console.log(pokemonGenerator.data);
+      });
+  };
+
+  // displayPokemon();
+  pokemonGenerator.displayPokemon = () => {
     console.log('display pokemon')
     // select header
-    const headerElement = document.querySelector("h1");
-    const pokemonName = pokemonGenerator.data.results[Math.floor(Math.random() * 248 +1)];
+    const headerElement = document.querySelector('h1');
 
-    console.log(pokemonName);
-    // get name from namespace object
+    // select image
+    const imgElement = document.querySelector('img')
 
+    // get random name and image from namespace object
+    const pokemonInfo = pokemonGenerator.data.results[Math.floor(Math.random() * 248 + 1)];
 
-//   pokemonGenerator.data.forEach ( (name) => {
-//     console.log(name);
-//   });
-  // loop through object and append name to header
+    console.log(pokemonInfo);
 
+    // fetching more data from random pokemon
+    const newURL = "https://pokeapi.co/api/v2/pokemon/" + pokemonInfo.name;
+
+    fetch(newURL)
+      .then((response) => {
+        // convert to .json
+        return response.json();
+      })
+      .then((jsonData) => {
+        pokemonGenerator.data2 = jsonData;
+        console.log(pokemonGenerator.data2);
+
+        // append image to page
+        // target img src
+      imgElement.src = pokemonGenerator.data2.sprites.front_default;
+
+        // target img alt
+      imgElement.alt = pokemonGenerator.data2.name;
+
+        // append img to page
+      imgElement.append();
+    });
+
+    // delete header element upon click of button
+    headerElement.innerText = ('');
+    
+    // append name to page
+    headerElement.append(pokemonInfo.name);
 };
 
-// select the image
-
-// get data from namespace object
-
-// loop thru data
-
-// create a dropdownElement
-
-// create headerElement
-
-// create imageElement
-
-// create randomizeButton
-
-// set value of dropdownElement
-
-// set value of headerElement
-
-// set value of imageElement
-
-// set value of randomizeButton
-
-// append array of pokemon to dropdownElement
-
-// append pokemon name to headerElement
-
-// append image to imageElement
-
-// append data to page from randomizeButton on click
-
 // call init method!
-
 pokemonGenerator.init = () => {
   pokemonGenerator.getData();
 };
 
 pokemonGenerator.init();
+});
+
+
+
+
