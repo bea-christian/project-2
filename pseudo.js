a9763ce67b39ac78f1a29e6d4d43b493ba27a20f
Christian Yuen <christianyuen1@gmail.com>
2023-01-11 19:04:55 -0500

    Added Styles + Appended Pokemon Stats

--- pseudo.js
+++ pseudo.js
@@ -44,6 +44,21 @@ console.log(event);
     // select image
     const imgElement = document.querySelector('.pokemonImage')
 
+    // select weight
+    const weightElement = document.querySelector('.weight');
+
+    // select height
+    const heightElement = document.querySelector('.height');
+
+    // select hp
+    const hpElement = document.querySelector('.hp');
+
+    // select attack
+    const attackElement = document.querySelector('.attack');
+
+    // select defense
+    const defenseElement = document.querySelector('.defense');
+
     // get random name and image from namespace object
     const pokemonInfo = pokemonGenerator.data.results[Math.floor(Math.random() * 248 + 1)];
 
@@ -70,13 +85,45 @@ console.log(event);
 
         // append img to page
       imgElement.append();
+
+      // append weight to page
+      weightElement.append('weight: ',pokemonGenerator.data2.weight,' lbs');
+
+      // append height to page
+      heightElement.append('height: ', pokemonGenerator.data2.height, ' feet');
+
+      // append hp to page
+      hpElement.append('hp: ',pokemonGenerator.data2.stats[0].base_stat);
+
+      // append attack to page
+      attackElement.append('attack: ', pokemonGenerator.data2.stats[1].base_stat);
+
+      // append hp to page
+      defenseElement.append('defense: ', pokemonGenerator.data2.stats[2].base_stat);
+
     });
 
     // delete header element upon click of button
     headerElement.innerText = ('');
-    
+
+    // delete weight element upon click of button
+    weightElement.innerText = ('');
+
+    // delete height element upon click of button
+    heightElement.innerText = ('');
+
+    // delete hp element upon click of button
+    hpElement.innerText = ('');
+
+    // delete attack element upon click of button
+    attackElement.innerText = ('');
+
+    // delete defense element upon click of button
+    defenseElement.innerText = ('');
+
     // append name to page
     headerElement.append(pokemonInfo.name);
+    
 };
 
 // call init method!
